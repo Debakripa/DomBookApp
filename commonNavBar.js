33323a835bff50e60bbd67d54f18dd771e5e80fa
Debakripa <computer.debakripa@gmail.com>
2024-12-28 15:44:07 +0530

    Frontend Repo

---
+++ commonNavBar.js
@@ -0,0 +1,4 @@
+const navBar = `<nav><ul><li><a href="index.html">Home</a></li><li><a href="admin.html">Admin</a></li><li><a href="books.html">Books</a></li></ul ></nav > `;
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('navBar').innerHTML = navBar;
+});
